50abcf43bde4b167d87f5da099eb05a98d450ac7
이진호 <spris1513@gmail.com>
2021-09-05 14:14:57 +0900

    주소도 실제 데이터 반영 / 나이정보까지 row작업

--- app/src/main/java/com/neppplus/listview_20210905/adapters/StudentAdapter.kt
+++ app/src/main/java/com/neppplus/listview_20210905/adapters/StudentAdapter.kt
@@ -41,8 +41,12 @@ class StudentAdapter(
 
 //        row의 내부에서, 필요한 텍스트뷰 등 UI요소들을 가져오자.
         val nameTxt = row.findViewById<TextView>( R.id.nameTxt )
+        val adressTxt = row.findViewById<TextView>(R.id.addressTxt)
+        val ageTxt = row.findViewById<TextView>(R.id.ageTxt)
 
         nameTxt.text = data.name
+        adressTxt.text = data.address
+
 
 //        만들어진 row를 getView 작업의 결과물로 지정.
 
