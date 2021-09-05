ae967a97f7aed3265aece4d0dae5e90a0d883f72
이진호 <spris1513@gmail.com>
2021-09-05 12:30:12 +0900

    Adapter Calss 생성 + 상속받기  - ArrayAdapter는 ()를 지원 X, 에러발생

---
+++ app/src/main/java/com/neppplus/listview_20210905/adapters/StudentAdapter.kt
@@ -0,0 +1,7 @@
+package com.neppplus.listview_20210905.adapters
+
+import android.widget.ArrayAdapter
+import com.neppplus.listview_20210905.datas.StudentData
+
+class StudentAdapter:ArrayAdapter<StudentData>(){
+}
