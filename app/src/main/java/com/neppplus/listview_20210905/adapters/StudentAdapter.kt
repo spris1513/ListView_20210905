6b0c41ab731a1d9fed0f2b4fbac567fa6b65f595
이진호 <spris1513@gmail.com>
2021-09-05 12:39:21 +0900

    받아온 재료들을 > 부모에게 전달  → 에러 사라짐

--- app/src/main/java/com/neppplus/listview_20210905/adapters/StudentAdapter.kt
+++ app/src/main/java/com/neppplus/listview_20210905/adapters/StudentAdapter.kt
@@ -8,5 +8,5 @@ import com.neppplus.listview_20210905.datas.StudentData
 class StudentAdapter(
     val mContext: Context,
     val resId: Int,
-    val mList: ArrayList<StudentData>) : ArrayAdapter<StudentData>(){
+    val mList: ArrayList<StudentData>) : ArrayAdapter<StudentData>(mContext, resId, mList){
 }
