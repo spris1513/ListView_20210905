dc01633595413dc4f7438a29980c13af4bffd222
이진호 <spris1513@gmail.com>
2021-09-05 12:35:50 +0900

    StudentAdapter를 실제로 쓰려면, 필요한 재료들을 요청.   - 왜? ArrayAdapter에게 넘겨줘야 동작되어서.

--- app/src/main/java/com/neppplus/listview_20210905/adapters/StudentAdapter.kt
+++ app/src/main/java/com/neppplus/listview_20210905/adapters/StudentAdapter.kt
@@ -1,7 +1,12 @@
 package com.neppplus.listview_20210905.adapters
 
+import android.content.Context
+import android.content.LocusId
 import android.widget.ArrayAdapter
 import com.neppplus.listview_20210905.datas.StudentData
 
-class StudentAdapter:ArrayAdapter<StudentData>(){
+class StudentAdapter(
+    val mContext: Context,
+    val resId: Int,
+    val mList: ArrayList<StudentData>) : ArrayAdapter<StudentData>(){
 }
