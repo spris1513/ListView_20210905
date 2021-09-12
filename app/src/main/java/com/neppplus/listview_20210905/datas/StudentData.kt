9bd52dd3df9136fe0f9b7e9ef2a00cb11adef964
이진호 <spris1513@gmail.com>
2021-09-12 10:18:25 +0900

    학생클래스 - 한국 나이 계산 기능(함수) 추가

--- app/src/main/java/com/neppplus/listview_20210905/datas/StudentData.kt
+++ app/src/main/java/com/neppplus/listview_20210905/datas/StudentData.kt
@@ -6,4 +6,17 @@ class StudentData(
     val address: String) {
 
 
+//    이 사람의 나이를 계산해주는 기능
+//    연도를 넣으면 > 그 해의 나이를 계산
+
+    fun getKoreaAge( year: Int ) : Int {
+
+//    해당 연도의 한국 나이 계산
+        val koreanAge = year - this.birthYear + 1
+
+//        koreanAge가 최종 결과라고 지정.
+        return koreanAge
+
+    }
+
 }
